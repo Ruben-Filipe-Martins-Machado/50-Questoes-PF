c188bc0c30d95900963efae72c50c89afc3e3915
Ruben Machado <62147349+Ruben-Filipe-Martins-Machado@users.noreply.github.com>
2021-01-17 16:28:07 +0000

    Update 002.hs
    Assim a função funciona corretamente porque senão ela vai parar quando o y > z e da [ ], por isso é que no exemplo enumFromThenTo 1 3 10 só da [1,3,5,7] e desta forma consegue-se contornar o problema dando a resposta correta [1,3,5,7,9]

--- 002.hs
+++ 002.hs
@@ -1,3 +1,4 @@
 myEnumFromThenTo :: Int -> Int -> Int -> [Int]
-myEnumFromThenTo x y z | x > y || x > z || y > z = []
+myEnumFromThenTo x y z | x > y || x > z = []
+                       | y > z = [x]
                        | otherwise = x : myEnumFromThenTo y (y+y-x) z 
